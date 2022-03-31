78ae8759268aab2cfce30432a0ce63b3b2cd2afe
Ali <ali.majeed@devsinc.com>
2022-03-31 21:44:35 +0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get '', to:'posts#index'
+  get 'posts/index'
+  get 'posts/viewpage'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
