318cb955323d5ca2682b6ddd4ac2c4f88b543e70
Ali <ali.majeed@devsinc.com>
2022-04-01 18:45:24 +0500

    create and read implemented for Post Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,23 @@ class PostsController < ApplicationController
   end
   def viewpage
   end
+  def new
+    @post = Post.new()
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
