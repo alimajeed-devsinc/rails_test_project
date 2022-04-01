df680db0ebb7086e689dceb6468b5392fa935ae6
Ali <ali.majeed@devsinc.com>
2022-04-01 15:49:30 +0500

    articles controller added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get '', to:'posts#index'
   get 'posts/index'
   get 'posts/viewpage'
+
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
